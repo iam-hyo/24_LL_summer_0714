fe2c6655defa728758d31131f320f304b6159a2b
iam-hyo <hyojunj@naver.com>
2024-07-23 11:31:22 +0900

    .git삭제

---
+++ summer_3/12th_FE_API/src/components/Third/ThirdApi.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import axios from "axios";
+import { BtnWrapper, BtnContainer } from "../First/Styled";
+// import axios from 'axios';
+
+const ThirdApi = () => {
+  const [news, setNews] = useState([]);
+
+  //state 작성
+
+  //OPEN API 비동기로 불러와 state에 저장 및 불러오기
+  //이때의 url은 https://newsapi.org/v2/top-headlines?country=kr&apiKey=${import.meta.env.VITE_NEWS_API_KEY}
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`);
+      console.log(response);
+      // const sortedData = response.data.RealtimeCityAir.row.sort(
+      //   (a, b) => b.PM10 - a.PM10
+      // );
+      // console.log(sortedData);
+      // setWeather(sortedData);
+      setNews(response.data.articles);
+      
+      console.log(news)
+    } catch (error) {
+      console.log("에러!")
+    }
+};
+
+  return (
+    <>
+      <BtnWrapper>
+        <BtnContainer onClick={fetchData}>
+          데이터 GET해보기! <br />
+        </BtnContainer>
+      </BtnWrapper>
+    </>
+  );
+};
+
+export default ThirdApi;
